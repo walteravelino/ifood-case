9c258c9ce8b946e907fa3d95ad14b4511159886d
Walter Avelino <walter.avelin@gmail.com>
2025-08-16 20:09:28 -0300

    feat: round avg

--- analysis/questions/2.sql
+++ analysis/questions/2.sql
@@ -2,6 +2,8 @@ USE
 nyc_taxi.processed;
 
 -- 2) Média de passageiros (passenger_count) por cada hora do dia no mês de maio considerando todos os táxis da frota:
-SELECT *
+SELECT
+pickup_hour,
+ROUND(avg_passengers, 2) AS avg_passengers
 FROM gold_hourly_avg_passengers_may
 ORDER BY pickup_hour ASC;
